1fe6f3dd46e60e2edf6dd1eac503d35b3d16ac57
Avramutdaniel <Avramutdaniel@ymail.com>
2019-03-05 17:49:03 +0200

    Creating Statement Section and Services Section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,50 @@
 /* ============================================================
                         Preloader
 ===============================================================*/
-$(window).on('load', function() {
+$(window).on('load', function () {
     $('#status').delay(350).fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+/* ============================================================
+                        Team Section
+===============================================================*/
+
+$(function () {
+
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
+
+/* ============================================================
+                        Progress bars
+===============================================================*/
+$(function () {
+
+    $("#progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 1500);
+        });
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+        });
+});
+
+
+
+
+
+
+
